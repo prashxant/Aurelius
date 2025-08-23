484cab66bbeb2c3d139fc7181d0b59f99a895195
prashxant <notxdead0@gmail.com>
2025-08-23 18:55:40 +0530

    starting it

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div  className="  h-screen w-screen flex justify-center items-center bg-linear-to-b from-fuchsia-300 to-fuchsia-100">
+
+
+    
+
+    </div>
+  );
+}
