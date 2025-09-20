7be55ff30b0f1c58962ba14a238b2ad4804fd811
prashxant <notxdead0@gmail.com>
2025-09-20 20:38:18 +0530

    adding a rich text editor

---
+++ app/journal/page.tsx
@@ -0,0 +1,11 @@
+import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor"
+import { useRef } from "react"
+
+export default function Page() {
+
+
+
+  return (
+     <SimpleEditor />
+  )
+}
