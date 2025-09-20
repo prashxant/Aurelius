96aea419c65e2de19ecc2bfa7c5b7209e13e7440
prashxant <notxdead0@gmail.com>
2025-09-20 18:33:34 +0530

    using usrRef to get current?.value

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,17 +1,26 @@
-import React, { useRef } from 'react'
+'use client'
+import React, {  useRef } from 'react'
 
 function Write() {
-
+  const inputRef = useRef<HTMLInputElement>(null)
+  function handleSubmit(){
+    const message = inputRef.current?.value
+    alert(message)
+    return
+  }
 
 
 
   return (
     <div className='h-screen w-screen flex flex-col bg-blue-200 justify-center items-center'>
 
-      <input className='size-32 text-black bg-amber-200'type="text" />
+      <input ref={inputRef} className='size-32 text-black bg-amber-200'type="text" />
 
-      <button className='text-2xl text-black'>submit</button>
 
+
+
+      <button onClick={handleSubmit}
+      className='text-2xl mt-2 cursor-pointer rounded-md px-6 py-2 bg-blue-500 shadow-2xl text-black'>submit</button>
     </div>
   )
 }
