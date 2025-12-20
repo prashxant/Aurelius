32327aa4461495755c8b1f73fe3ffb5d074680a5
prashxant <notxdead0@gmail.com>
2025-12-20 18:25:32 +0530

    yo

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,12 +1,14 @@
 
 import MyEditorPage from '@/components/mainEditor';
-import React from 'react'
+
 
 
 export default function page () {
   return (
-    <div className="flex  flex-col gap-10 bg-black min-h-screen max-w-3xl mx-auto rounded-xl m-10 text-white ">
-      <MyEditorPage/>
+    <div className="flex flex-col gap-10   min-h-screenn mx-auto rounded-xl m-10 ">
+
+       <MyEditorPage/>
+
     </div>
   );
 }
